1cb512aad835953d8d67b38a6a40402901767921
david <david.kunavar@gmail.com>
2021-12-27 20:42:22 +0100

    naloge

--- main.js
+++ main.js
@@ -2,11 +2,10 @@
 function prva__prva(){
     let x = prompt("Vnesi ime");
     let y = document.getElementById("prva__prva")
-    y.innerText = "Pozdravljen " + x;
-}
+    y.innerText = 'Pozdravljen' + x}
 function prva__druga(){
     let x = Number(prompt("Vnesi stevilko:"));
-    let y = document.getElementById("prva__cetrta");
+    let y = document.getElementById("prva__druga");
     if(x > 0) {
         y.innerText = "Stevilka je pozitivna";
     }
@@ -17,13 +16,25 @@ function prva__druga(){
 function prva__tretja(){
    let s = Number(prompt("Vnesi pot"));
    let t = Number(prompt("Vnesi cas"));
+   let v = s/t;
    let y = document.getElementById("prva__tretja");
 
-    y.innerText = "Hitrost je " + s/t " m/s";
+    y.innerText = "Hitrost je" + v "m/s";
 }
 function prva__cetrta(){
     let s = Number(prompt("Vnesi visino pravokotnika"));
     let t = Number(prompt("Vnesi dolzino katete"));
+    let p = s*t/2;
     let y = document.getElementById("prva__cetrta");
-    y.innerText = "Ploscina je " + s*t/2 ;
+    y.innerText ="Povrsina je" + p;
+}
+function prva__peta(){
+    let x = Number(prompt("Vnesi stevilo"));
+    let y = document.getElementById("prva__peta");
+    if (x/2 == 0 && x/7 == 0){
+        y.innerText = "Je deljivo";
+    }
+    else{
+        y.innerText = "Ni deljivo";
+    }
 }
